c2ee021e97332c61aa11462f2c0e21c97f46b85b
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-02-12 22:32:22 +0700

    git add .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ``, // Viet cac th trong nay luon
-  // 1 component: teampalte Url quan trong nhat
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -12,25 +10,6 @@ export class AppComponent {
   isDanger = false;
   isWarning = false;
   classes="box red-border yellow-background";
-  // UserId - id - title - body
-  posts = [
-    {
-      id: 1,
-      userId: "012",
-      title: "AVC",
-      post: "ABCD"
-    },
-    {
-      id: 2,
-      userId: "023",
-      title: "ABC",
-      post: "avcxcc"
-    },
-    {
-      id: 3,
-      userId: "024",
-      title: "xc",
-      post: "admin"
-    }
-  ]
+
+  currentProgress = 70;
 }
